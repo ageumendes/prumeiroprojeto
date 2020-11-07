4e25a1cabb157dedf9ebd7955e3aac2bcb3508da
ageumendes <74105930+ageumendes@users.noreply.github.com>
2020-11-07 11:42:00 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+//var corbtn1 = window.document.getElementById("btn1")
+//var cordepart1 = window.document.getElementById("departamento1")
+/*function passamouse(cor) {   
+    cor.style.background ="rgb(202, 11, 11)"
+}
+function tirarmouse(cor) {
+    cor.style.background ="rgb(221, 21, 21)"
+}
+
+function clicou() {
+    alert("entrou!")
+}
+
+function pmDepart(cor) {
+    cor.style.background="rgb(240, 240, 240)"
+}
+function tmDepart(cor) {
+    cor.style.backgroundColor="rgb(255, 255, 255)"
+}
+function btn10ent(cor) {
+    cor.style.backgroundColor="rgb(221, 21, 21)"
+    cor.style.opacity="1"
+}
+function btn10sai(cor) {
+    cor.style.backgroundColor="white"
+    cor.style.opacity="0.5"
+}
+function entDepart() {
+    //alert("oi")
+    document.getElementById("body").style.backgroundColor = "black"
+    document.getElementById("body").style.transition="1s"
+    document.getElementById("body").style.opacity = "0.5"
+
+}
+function outDepart() {
+    document.getElementById("body").style.backgroundColor ="rgb(235, 231, 231)"
+    document.getElementById("body").style.opacity="1"
+}
+function relogio() {
+    var hratual = new Date()
+    var horas = hratual.getHours()
+    var minutos = hratual.getMinutes()
+    var segundos = hratual.getSeconds()
+    var milisegundos = hratual.getMilliseconds()
+
+    var ponteiroHoras = horas * 30 + minutos/2
+    var ponteiroMinutos  = minutos * 6 + (segundos*6)/60
+    var ponteiroSegundos = segundos * 6 + milisegundos/166
+    
+    
+    
+    if(horas < 10) {
+        horas = "0"+horas
+    }
+    if(minutos < 10){
+        minutos = "0"+minutos
+    }
+    if(segundos < 10) {
+        segundos = "0"+segundos
+    } 
+
+    document.getElementById("hrs").innerHTML = horas
+    document.getElementById("mnt").innerHTML = minutos
+    document.getElementById("sgd").innerHTML = segundos
+
+    document.getElementById("pontrelsgd").style.
+    document.getElementById("pontrelsgd").style.
+    document.getElementById("pontrelsgd").style.
+    document.getElementById("pontrelsgd").style.
+
+    /*while (segundos < 60) {
+        consol.log("ola, meu nome é Ageu")
+        segundos--
+    }
+
+    //pontrelsgd.style.transform = `rotate(${ponteiroSegundos}deg)`
+}
+
+
+window.setInterval("relogio()", 1000)
+*/
